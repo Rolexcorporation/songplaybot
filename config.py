ddb3018e40c7c8684cf928513c673b6b36efca49
Rolexcorporation <121960094+Rolexcorporation@users.noreply.github.com>
2023-01-04 23:45:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "26885114"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "41bb6d1f706da3bfbe95a820f427ae78" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5832249400:AAHz02Tf31BGOsXT7O6i-6PoEP1eUTPPdts" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
